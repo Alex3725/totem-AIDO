5df50e508438993bbbf7e3ec3a71f46586be6315
Dogo2009yt <107557595+Dogo2009yt@users.noreply.github.com>
2026-01-20 15:46:02 +0100

    Menù principale v1
    Menù di scelta iniziale versione 1.0

--- script.js
+++ script.js
@@ -4,15 +4,9 @@ let isIdle = false;
 const statusDiv = document.getElementById("status");
 function setIdle() {
   isIdle = true;
-  statusDiv.textContent = "User is not active";
-  statusDiv.style.color = "red";
   window.location.href = 'index.html';
 }
 function setActive() {
-  if (isIdle) {
-    statusDiv.textContent = "User is active";
-    statusDiv.style.color = "green";
-  }
   isIdle = false;
   clearTimeout(timeout);
   timeout = setTimeout(setIdle, idleTime);
