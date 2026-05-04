7dcc2ee0e593065cdb54ee15205cd21e566244c7
Alex3725 <alexprevitali637@gmail.com>
2026-05-04 16:48:12 +0200

    fix aurico

--- progettoTotemAido/src/lib/stores/menu-dots.store.ts
+++
@@ -1,8 +0,0 @@
-import { writable } from 'svelte/store';
-import type { MenuOptionSlug } from '$lib/data/menu-options';
-
-export const menuDotsSlug = writable<MenuOptionSlug>('faq');
-
-export function setMenuDotsSlug(slug: MenuOptionSlug) {
-	menuDotsSlug.set(slug);
-}
