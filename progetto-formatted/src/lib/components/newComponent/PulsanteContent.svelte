873a7754814506b94c0737496f8ad4e624d7a193
Alex3725 <alexprevitali637@gmail.com>
2026-05-04 16:29:36 +0200

    fix rooting dots e animazione 02

--- progetto-formatted/src/lib/components/newComponent/PulsanteContent.svelte
+++ progetto-formatted/src/lib/components/newComponent/PulsanteContent.svelte
@@ -12,7 +12,7 @@
     class=" rounded-[1.2cqw] bg-white px-[4.25cqw] py-[1.6cqh]
            text-[4.7cqw] font-black leading-none text-[#121212]
            shadow-[0_0.55cqw_0.75cqw_rgba(0,0,0,0.38)] flex items-center "
-  onclick={handleClick}
+  on:click={handleClick}
 >
   {titolo}
 </button>
