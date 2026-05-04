4a26547d2478e5e3ecf307a49d2dd61f56a73c8a
David Mistri <davidmistri08@gmail.com>
2026-05-04 17:21:14 +0200

    sigma gioco

--- progetto-formatted/src/routes/opzioni-menu/impara-sium/pagina-content/+page.svelte
+++ progetto-formatted/src/routes/opzioni-menu/impara-sium/pagina-content/+page.svelte
@@ -3,7 +3,7 @@
 
 <div class="flex h-full w-full select-none flex-col items-center rounded-[1.45cqw] bg-[#d40000] px-[2.75cqw] pb-[2.2cqh] pt-[2.8cqh] text-center text-white">
     
-    <h1>Sium</h1>
+    <iframe src="/iframe/gameIFrame/index.html" title="mappa sedi AIDO" frameborder="0" width="100%" height="10000" ></iframe>
 
 </div>
  
